606e2eb13f2beb90b298df028f38be9fa1470665
Anton <80984465+Antongo22@users.noreply.github.com>
2023-11-26 22:48:16 +0300

    Добавьте файлы проекта.

---
+++ tasks/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tasks
+{
+    internal class Program
+    {
+        void Commmand 
+
+
+        static void Main(string[] args)
+        {
+            int actionCode = 1;
+
+            while(actionCode != 0)
+            {
+                if(int.TryParse(Console.ReadLine(), out int ac))
+                {
+                    actionCode = ac;
+
+                }
+                else
+                {
+                    Console.WriteLine("Неверные ");
+                }
+            }
+        }
+    }
+}
